15cf3d441e68fc3510cce9e2d36ccd2241c8db04
leandromccordeiro <leandroo_marcos@yahoo.com.br>
2024-11-12 14:40:28 -0300

    install dependency and create Chirp component

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -16,7 +16,9 @@ class ChirpController extends Controller
     public function index()
     {
         // return response('cheguei aqui');
-        return Inertia::render('Chirps/Index', []);
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
     }
 
     /**
